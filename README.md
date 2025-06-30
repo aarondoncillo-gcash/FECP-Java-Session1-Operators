1022d165eadf721ef49f652c24e74d809efa8a4c
Ronan Aaron Doncillo <ronanaaron.doncillo@gcash.com>
2025-06-30 15:21:34 +0800

    Final Submission, Lab 1: Celsius to Fahrenheit Converter

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Name: Ronan Aaron Doncillo
 # Lab 1: Celsius to Fahrenheit Converter
+## Screenshot of output
+![Lab 1 output proof](lab 1 output.png "Lab 1 output proof")
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
+import java.util.Scanner;
 
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        float celsius;
+        // Get celsius input
+        System.out.print("Enter temperature in Celsius: ");
+        celsius = sc.nextFloat();
+        // Fahrenheit input
+        float fahrenheit = celsius * (9/5.0f) + 32;
+        //Display Fahrenheit output
+        System.out.printf("Temperature in Fahrenheit: %.2f", fahrenheit );
     }
 }
